0083bc7a62695fbb3a03884596c996f529dc443a
jordibaselga <jobaselga@gmail.com>
2022-02-14 17:25:21 +0100

    paso comanda

--- src/App.js
+++ src/App.js
@@ -1,36 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ListaProductos from './components/ListaProductos';
-import getProductos from './services/getProductos';
 
 
 
 function App() {
-  const [productos, setProductos] = useState([])
-  //'All / Saleable / PoS / Food'
-  //'All / Consumable / Bebidas'
-  const comidas = 'All / Saleable / PoS / Food'
-  const bebidas = 'All / Consumable / Bebidas'
-  let categoriaKeyword = comidas
-  useEffect(function () {
-    getProductos({ categoriaKeyword }).then(productos => setProductos(productos))
-  })
+  const [keyword, setKeyword] = useState('All / Consumable / Bebidas')
 
   return (
     <div className="App">
       <nav>
-        <button onClick={() => {
-          categoriaKeyword = bebidas
-          console.log(categoriaKeyword);
-        }}>PARA BEBER</button>
-
-        <button onClick={() => {
-          categoriaKeyword = comidas
-          console.log(categoriaKeyword);
-        }}>PARA COMER</button>
+        <button onClick={() => setKeyword('All / Consumable / Bebidas')}>PARA BEBER</button>
+        <button onClick={() => setKeyword('All / Saleable / PoS / Food')}>PARA COMER</button>
       </nav>
       <section className="App-section">
-        <ListaProductos productos={productos} />
+        <ListaProductos keyword={keyword} />
       </section>
     </div>
   );
